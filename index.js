22c240a86d0290cbfc69b8628f457624f70771a7
Manan Jain <writetomananjain21@gmail.com>
2024-07-13 00:11:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const port=8080;
 const path=require("path");
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({extended:true})); //middleware that parses incoming urlencoded requests.
 
@@ -12,14 +13,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts=[
     {
+    id:uuidv4(),
     username:"Manan Jain",
     content:"Good in Maths",
    },
    {
+    id:uuidv4(),
     username:"Lakhya Gupta",
     content:"Good At Fitness",
    },
    {
+    id:uuidv4(),
     username:"Sarthak Gupta",
     content:"Good in Painting",
    }
@@ -37,6 +41,12 @@ app.post("/posts",(req,res)=>{
     let{username,content}=req.body;
     posts.push({username,content});
     res.redirect("/posts");
+});
+
+app.get("/posts/:id",(req,res)=>{
+    let{id}=req.params;
+    let post=posts.find((p)=>id===p.id);
+    res.render("show.ejs",{post});
 });
 
 app.listen(port,()=>{
