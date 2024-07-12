166689469d9f08a6e0fc1fe645fa1e840b25add5
Manan Jain <writetomananjain21@gmail.com>
2024-07-12 10:31:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ let posts=[
 app.get("/posts",(req,res)=>{
     res.render("index.ejs",{ posts });
 });
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    let{username,content}=req.body;
+    posts.push({username,content});
+    res.redirect("/posts");
+});
+
 app.listen(port,()=>{
     console.log("listening requests on port 8080");
 });
